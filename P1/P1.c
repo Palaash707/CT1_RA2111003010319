619be8951208ad8d7901a17c3fb99e05fc8fd0f5
Palaash707 <94762404+Palaash707@users.noreply.github.com>
2022-04-20 21:19:07 +0530

    Add files via upload

--- P1/P1.c
+++ P1/P1.c
@@ -7,8 +7,8 @@ int main() {
     scanf("%d",&bill_amount);
     printf("The amount given is: ");
     scanf("%d",&amount_given);
-    q=bill_amount/amount_given;
-    r=bill_amount%amount_given;
+    q=amount_given/bill_amount;
+    r=amount_given%bill_amount;
     printf("the quotient is: %d\n",q);
     printf("the remainder is: %d",r);
 
